c16f245fed0ace0225169f5b532649876ec85af4
cd2ong <c.david.ong@gmail.com>
2020-03-20 11:35:40 -0400

    Completed modules 1-4 of Angular Cli course

---
+++ my-app7/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+const num = 7;
+
+@Component({
+  selector: 'jp-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Angular CLI Course!';
+
+  sayHello() {
+    const x = 10;
+  }
+}
